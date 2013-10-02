1c67884fe9c03d30c2dfdf9718d36304df46ba28
MetaLang <jared771@gmail.com>
2013-10-02 16:14:59 -0300

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyQuest::Application.config.secret_key_base = '24ac15a98d947ced341470800e98547c9ddd5454b88523800c05d40d4174ce0d4f02d072219bda8d1a2dd68fcc9fce98dec179e88deb97a8e58a745078b61a3b'
