5fe285eed595aec9b94bef2cd6926819425bf86e
Zouroborus <ethan.gaudet@hotmail.com>
2013-11-24 23:06:54 -0400

    New users form
    Added a new users form (still needs a lot of work, it doesn't work, none
    of the labels really display because it is black text on a black
    background)

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ RubyQuest::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
 
   # Sample resource route with options:
   #   resources :products do
