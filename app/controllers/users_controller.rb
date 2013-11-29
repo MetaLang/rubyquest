29557e1408c02179ad3f03dc391635747837fc14
Zouroborus <ethan.gaudet@hotmail.com>
2013-11-28 20:47:32 -0400

    Create Users
    Changed the routing so that the home page is now home/login. Can create
    users at users/new, and when user is created it redirects to the home
    page with a flash message saying the user is created. (No validation or
    anything)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
 	def new
 	end
+	
+	def create
+		@user = User.new(params[:user])
+		@user.save
+		flash[:alert] = "You are not yet assigned to an experiment."
+		redirect_to "http://localhost:3000", alert: "User created!"
+	end
+	
+	private
+		def user_params
+			params.require(:user).permit(:username, :password, :email)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RubyQuest::Application.routes.draw do
-  get "home/index"
+  get "home/login"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ RubyQuest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'home#login'
 
   # See how all your routes lay out with "rake routes"
 
