e9b3850404e5565c6d61349d3c10378de43f7a0d
Zouroborus <ethan.gaudet@hotmail.com>
2013-11-28 21:39:04 -0400

    Link to new users
    Added link on the login page to create a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RubyQuest::Application.routes.draw do
   get "home/login"
-
+	get "users", to: "users#new"
+	
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
