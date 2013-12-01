5c9a398f73dc47f09b2888401b3a5a0d2da7513a
MetaLang <jared771@gmail.com>
2013-12-01 18:01:00 -0400

    Changed UsersController#create to use create for creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 	def create
-		@user = User.new(params[:user])
-		@user.save
+		@user = User.create(params[:user])
 		flash[:alert] = "You are not yet assigned to an experiment."
 		redirect_to root_url, alert: "User created!"
 	end
