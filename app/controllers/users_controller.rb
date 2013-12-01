d299f9ce1ae1a11b13848da192c5564e103c4955
Ellery Furlong <ellery.furlong@outlook.com>
2013-12-01 16:19:20 -0400

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,6 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 		@user.save
 		flash[:alert] = "You are not yet assigned to an experiment."
-		
-		if @user.save
-			redirect_to home_url, alert: "User created!"
-
-		end
-		
-		
-
 	end
 
 	def login
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :inventory_id, :password, :room_id, :session_id, :username
   has_one :inventory
 
-  validates :password, 	:presence => true,
-						:length => { 	:minimum => 5,
-										:maximum => 12 }
-  end
-  validates :username, 	:presence => true, 
-						:length => { 	:minimum => 2,
-										:maximum => 12 }
-  end
+  validates_presence_of :email, :password, :username
+
 end
