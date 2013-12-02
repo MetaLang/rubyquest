1b9880098ac10bee692b8869756337dd499aacc5
Zouroborus <ethan.gaudet@hotmail.com>
2013-12-01 22:09:38 -0400

    Login validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,17 @@ class SessionsController < ApplicationController
 	def create
 		user = User.where(username: params[:session][:username]).first
 		#if user && user.authenticate(params[:session][:password])
-		if user.password == params[:session][:password]
+		if user == nil
+			flash.now[:error] = 'Please enter your username and password'
+			render 'sessions/new'
+		elsif user.password == params[:session][:password]
 			# Sign the user in and redirect to the user's show page.
 			sign_in user
 			redirect_to root_url
 		else
 			# Create an error message and re-render the signin form.
 			flash.now[:error] = 'Invalid email/password combination'
+			render 'sessions/new'
 		end
 	end
 end
