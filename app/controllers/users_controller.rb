26780d15e1999fb20a28620ab0fa0c55fd561ae9
Ellery Furlong <ellery.furlong@outlook.com>
2013-12-01 21:26:48 -0400

    validation final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,14 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 		@user.save
 		flash[:alert] = "You are not yet assigned to an experiment."
+		
+		if @user.save
+			redirect_to home_url, alert: "User created!"
+		end
+		
+		if @user.errors.any?
+			render 'new'
+		end
 	end
 
 	def login
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
   has_one :inventory
 
   validates_presence_of :email, :password, :username
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_length_of :password, :username, :maximum => 12, :minimum => 3
+  validates_uniqueness_of :email, :username,
+	:message => " is already used"
 
 end
