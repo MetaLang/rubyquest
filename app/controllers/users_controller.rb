96037ec692959fb3361b0401f959a7d04f8af42c
Ellery Furlong <ellery.furlong@outlook.com>
2013-12-01 21:57:31 -0400

    fixed merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 	def create
 		@user = User.create(params[:user])
 		flash[:alert] = "You are not yet assigned to an experiment."
-<<<<<<< HEAD
 		
 		if @user.save
 			redirect_to home_url, alert: "User created!"
@@ -11,9 +10,8 @@ class UsersController < ApplicationController
 		if @user.errors.any?
 			render 'new'
 		end
-=======
-		redirect_to root_url, alert: "User created!"
->>>>>>> 5c9a398f73dc47f09b2888401b3a5a0d2da7513a
+
+
 	end
 	
 	private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,24 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :inventory_id, :password, :room_id, :session_id, :username
-  has_one :inventory
-<<<<<<< HEAD
+	attr_accessible :email, :inventory_id, :password, :room_id, :session_id, :username
+	has_one :inventory
 
-  validates_presence_of :email, :password, :username
-  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates_length_of :password, :username, :maximum => 12, :minimum => 3
-  validates_uniqueness_of :email, :username,
-	:message => " is already used"
 
-end
-=======
+	validates_presence_of :email, :password, :username
+	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+	validates_length_of :password, :username, :maximum => 12, :minimum => 3
+	validates_uniqueness_of :email, :username,
+		:message => " is already used"
+
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	
 	def User.new_remember_token
     SecureRandom.urlsafe_base64
-  end
+	end
 
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
 	
 	private
 
@@ -29,4 +27,4 @@ end
 			self.remember_token = User.encrypt(User.new_remember_token)
 		end
 end
->>>>>>> 5c9a398f73dc47f09b2888401b3a5a0d2da7513a
+
