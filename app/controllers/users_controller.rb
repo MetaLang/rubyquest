f797d4b1a04d5769e5567ffd274f6e99dced9484
Zouroborus <ethan.gaudet@hotmail.com>
2013-12-01 22:27:03 -0400

    Fixed redirect after user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
 	def create
 		@user = User.create(params[:user])
-		flash[:alert] = "You are not yet assigned to an experiment."
 		
 		if @user.save
-			redirect_to home_url, alert: "User created!"
+			redirect_to root_url, alert: "User created!"
 		end
 		
 		if @user.errors.any?
