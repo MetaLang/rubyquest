6bfb84c3e11fd356b7875787cfea72738f173037
MetaLang <jared771@gmail.com>
2013-12-04 14:08:33 -0400

    Minor changes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,10 @@
 class RoomsController < ApplicationController
     def show
         @room = Room.where(id: params[:id]).first
-        @itemsInRoom = unless @room.nil?
-            Item.where("id = ?", @room.read_attribute(:id))
-        else
-            []
-        end
-    end
+    #     @itemsInRoom = unless @room.nil?
+    #         Item.where("id = ?", @room.read_attribute(:id))
+    #     else
+    #         []
+    #     end
+end
 end
